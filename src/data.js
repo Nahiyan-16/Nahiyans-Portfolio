2009645ba3b68ed4040798ff314c0df773763f59
Nahiyan16 <nahiyanahmed11@gmail.com>
2024-01-07 12:15:19 -0500

    Updated about me section

--- src/data.js
+++ src/data.js
@@ -47,4 +47,7 @@ export default {
     image: webCrawlerImg,
     backgroundColor: "white",
   },
+  about: {
+    text: "My name is Nahiyan, I am a recent graduate of Queens College with a degree in computer science. I am a skilled web developer with experience in several programming languages, including JavaScript, Java, C++, and ReactJS. Throughout my time in college and at my internship at Nanotronics Imaging, I was able to work on several projects, both individually and in a team setting, which helped me to gain hands-on experience and develop a strong understanding of web development. I have a good understanding of front-end development and have a good grasp of JavaScript and ReactJS. I also have experience working with backend development and I have a good understanding of Java and C++. I am now seeking employment opportunities as a web developer to utilize my knowledge and skills to contribute to the growth and success of a company. I am excited to continue learning and growing as a developer in the field. I am open to learning new technologies and programming languages to stay current with the fast-paced industry. I am a quick learner and I am always eager to take on new challenges. I am confident that with my skills, I will be able to make meaningful contributions to any team that I will be a part of.",
+  },
 };
